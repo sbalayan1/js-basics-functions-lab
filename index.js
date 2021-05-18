2684d0fd01254198781ed3799afb4b46b92fa190
sbalayan1 <balayans2014@yahoo.com>
2021-05-17 20:37:38 -0700

    commit tree dce44e3fe4770ad985fc14aa758dafe91a61d301

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+    if (x>=42) {
+        return x - 42;
+    } else {
+        return 42 - x;
+    }
+    
+}
+
+function distanceFromHqInFeet(x) {
+    if (x>=42) {
+        return (x-42)*264;
+    } else {
+        return (42-x)*264;
+    }
+}
+
+function distanceTravelledInFeet(x,y) {
+    if (y>=x) {
+    return (y-x)*264;
+    } else {
+        return (x-y)*264;
+    }
+}
+
+
+//having issues with order of operations. function seems to work partially however 
+function calculatesFarePrice(x,y) {
+    if (distanceTravelledInFeet(x,y)<=400) {
+        return 0;
+
+    } else if (distanceTravelledInFeet(x,y)<=2000) {
+        return (distanceTravelledInFeet(x,y)-400)*0.02;  
+    
+    } else if (distanceTravelledInFeet(x,y)>2500) {
+        return 'cannot travel that far';
+
+    } else if (distanceTravelledInFeet(x,y)>2000) {
+        return 25;
+    
+    }
+}
